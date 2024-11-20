8800e87e4e9ce1c3f5135f188c2b3605b70c1ee2
delk73 <dan.elkins@gmail.com>
2024-11-20 08:16:50 -0800

    aa

--- src/js/shaderModules/circleModule.js
+++ src/js/shaderModules/circleModule.js
@@ -3,17 +3,12 @@ import { ShaderModule } from '../ShaderModule';
 export const createCircleModule = () => new ShaderModule('Circle', {
     uRadius: { 
         value: 0.3, 
-        control: { min: 0.1, max: 0.8 },
+        control: { min: 0.1, max: 0.8, step: 0.01 },  // Added fine step control
         label: 'Radius'
     },
     uStroke: { 
         value: 0.001, 
-        control: { min: -1.0, max: 1.0 },
+        control: { min: -1.0, max: 1.0, step: 0.001 },  // Added fine step control
         label: 'Stroke Width'
-    },
-    uStep: {
-        value: 16.0,
-        control: { min: 8.0, max: 32.0, step: 8.0 },
-        label: 'Grid Size'
     }
 });
